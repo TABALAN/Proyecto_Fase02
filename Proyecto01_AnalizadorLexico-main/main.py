c2f953b339c7757654c42c5b064c56b745215fd6
Alexander Balán <tabalan@correo.url.edu.gt>
2026-04-18 12:50:50 -0600

    1v

---
+++ Proyecto01_AnalizadorLexico-main/main.py
@@ -0,0 +1,22 @@
+#Instancias
+from lexer import Lexer
+from adaptadorLexer import adaptadorL
+from parser import parserin
+
+def main():
+    #Etapa 1: Analizador Léxico
+    archivo = "text1.txt"
+    print("------- Analizador Lexico -------")
+    lec = Lexer()
+    lec.leerArchivo(archivo)
+    lec.imprimirTokens()
+    
+    #Etapa 2: Parserin
+    print("\n\n------- Analizador sintactico -------")
+    parser = parserin()
+
+    #Ejecutar parserin
+    resultado = parser.analizar(lec.getTokens())
+
+if __name__ == "__main__":
+    main() 
