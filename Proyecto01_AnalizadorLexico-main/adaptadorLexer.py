d94a1d736eee555bef3e46bd3580ffe18ff6195e
Alexander Balán <tabalan@correo.url.edu.gt>
2026-04-18 12:44:49 -0600

    Primera versión con implementación de PLY

---
+++ Proyecto01_AnalizadorLexico-main/adaptadorLexer.py
@@ -0,0 +1,91 @@
+# Diccionarios de mapeo
+KEYWORDS = {
+    'int', 'float', 'if', 'else', 'while', 'return',
+    'and', 'switch', 'do', 'not', 'for', 'default',
+    'case', 'boolean', 'try', 'catch', 'or', 'main',
+    'elif', 'print', 'input', 'Read', 'def', 'mul'
+}
+
+COMP = {
+    '==': 'EQ', '!=': 'NEQ', '<=': 'LEQ', '>=': 'GEQ',
+    '<':  'LT', '>':  'GT',  '%':  'MOD',
+    '++': 'INC', '--': 'DEC', '+=': 'PLUSEQ', '-=': 'MINUSEQ'
+}
+
+ESP = {
+    '(': 'LPAREN', ')': 'RPAREN',
+    '{': 'LBRACE', '}': 'RBRACE',
+    '=': 'ASSIGN', '!': 'NEG',
+    '&': 'AMP',    '~': 'TILDE',
+    '¬': 'NEG',    '°': 'DEG'
+}
+
+PUNT = {
+    ',': 'COMMA', ':': 'COLON', ';': 'SEMI'
+}
+
+OP = {
+    '+': 'PLUS', '-': 'MINUS',
+    '*': 'TIMES', '/': 'DIVIDE'
+}
+
+CP = {
+    'Salto': 'NEWLINE'
+}
+
+class Token:
+    #Constructor perrón
+    def __init__(self, diccionario):
+        lexema = diccionario['lexema']
+        self.tipo = diccionario['tipo']
+
+        if self.tipo == 'key':
+            self.type = lexema.upper()
+        elif self.tipo == 'num':
+            self.type = 'NUM'
+        elif self.tipo == 'decimal':
+            self.type == 'DECIMAL'
+        elif self.tipo == 'Salto':
+            self.type = CP.get(lexema, 'CP')
+        elif self.tipo == 'comp':
+            self.type = COMP.get(lexema, 'COMP')
+        elif self.tipo == 'esp':
+            self.type = ESP.get(lexema, 'ESP')
+        elif self.tipo == 'punt':
+            self.type = PUNT.get(lexema, 'PUNT')
+        elif self.tipo == 'op':
+            self.type = OP.get(lexema, 'OP')
+        else:
+            self.type = self.tipo.upper()  
+
+        self.value = diccionario['lexema']
+        self.lineno = diccionario['linea']
+        self.lexpos = diccionario['columna']
+
+class adaptadorL:
+    def __init__(self, listaTokens):
+        #Se convierten todos los diccionarios de token a objetos Token
+        self.tokens = [Token(tokensito) for tokensito in listaTokens]
+        self.pos = 0
+        self.pendiente = None #Para manejar recuperación
+        self.parser = None
+
+    def reinsert(self, token):
+        self.pendiente = token   #Para guardar el token 
+
+    def token(self):
+        if self.pendiente is not None:   #revisar primero
+            tok = self.pendiente
+            self.pendiente = None
+            return tok
+        while self.pos < len(self.tokens):
+            tokenito = self.tokens[self.pos]
+            self.pos += 1
+            #Saltar identación porque nos causa clavos
+            if tokenito.tipo in ('INDENT', 'DEDENT'):
+                continue
+            tokenito.parser = self.parser
+            tokenito.lexer = self
+            return tokenito
+        return None
+    
